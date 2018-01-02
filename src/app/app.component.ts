92571cb9b09d4e4e9614ee93bc7b3e6df03fa2e1
Prashant Yadav <prashant_yadav@external.mckinsey.com>
2018-01-02 17:36:25 +0530

    ngSwitch directive USE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,8 @@ export class AppComponent {
     isSelected: false
   };
 
+  viewMode = 'map';
+
   onFavoriteChanged(eventArgs: FavoriteChangedEventArgs ) {
     console.log('favorite changed', eventArgs);
   }
